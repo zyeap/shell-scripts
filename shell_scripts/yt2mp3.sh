2aedb86554d83f2e8c8b226e2a455465bf8d5424
Zach Yeap <zachary.yeap1@gmail.com>
2021-08-26 11:11:17 -0700

    Added package check and mp3 download/convert function

--- shell_scripts/yt2mp3.sh
+++ shell_scripts/yt2mp3.sh
@@ -2,3 +2,46 @@
 
 # YouTube to mp3 downloader script
 # Requires youtube-dl and ffmpeg 
+
+URL=$1
+
+check_packages(){
+    PKGS="youtube-dl ffmpeg"
+    rc=0
+    for pkg in $PKGS
+    do
+        # which $pkg will search for existence of package 
+        # redirect stdout and stderr to /dev/null to bury output
+        # below is shorthand for cmd > /dev/null 2>&1
+        which $pkg &> /dev/null    
+
+        # $? will get the exit status of the last command
+        # echo $? to visually see exit status, 0 = success, 1 = failure
+        if [[ $? -eq 1 ]]; then
+            echo "$pkg package is not installed, please install $pkg for script to run" 
+            rc=1
+        fi
+    done
+    return $rc
+}
+
+get_mp3() {
+    youtube-dl -f bestaudio $URL --extract-audio --audio-format mp3
+}
+
+# run check_packages and check exit status
+check_packages
+if [[ $? -eq 1 ]]; then
+    echo "Exiting program..."
+    exit
+else
+    if [[ -z $URL ]]; then
+        echo "Please provide URL"
+        exit
+    else
+        get_mp3
+    fi
+fi
+
+
+
