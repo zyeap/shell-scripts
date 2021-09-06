ca3a39d0aece9a382a52dddde45243d53384f681
Zach Yeap <zachary.yeap1@gmail.com>
2021-09-06 14:27:38 -0700

    Added compression util

---
+++ shell_scripts/compression_util.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# compression assistant utility
+# take any directory and archive/compress or unarchive/extract as needed
+
+# print out explanation of how this works
+
+# get user input for full directory path
+# enter destination of output file (leave empty for current dir)
+
+# if file ends in .tgz, .zip, .gz, .xz, etc...... , perform unarchive/extract
+
+# else, perform archive/compress
